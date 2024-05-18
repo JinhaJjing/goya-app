b19a83f243e2813d294935674c763f591dcbcbb1
JinhaJjing <tjwlsgkkgslwjt@gmail.com>
2024-05-18 19:44:05 +0900

    업데이트

--- src/main/kotlin/com/group/goyaapp/util/GoogleSheets.kt
+++ src/main/kotlin/com/group/goyaapp/util/GoogleSheets.kt
@@ -12,28 +12,22 @@ import com.google.api.services.sheets.v4.SheetsScopes
 import com.google.api.services.sheets.v4.model.ValueRange
 import java.io.IOException
 
-const val CREDENTIALS_FILE_PATH: String = "google_sheet/google_spread_sheet_key.json"
-const val SHEET_ID: String = "1WobN-MMi-Nigpe19jeNTL7J377Ffpy3LPnjHB0qLwu4"
-const val APPLICATION_NAME: String = "goya-google-sheet"
+const val CREDENTIALS_FILE_PATH = "google_sheet/google_spread_sheet_key.json"
+const val SHEET_ID = "1WobN-MMi-Nigpe19jeNTL7J377Ffpy3LPnjHB0qLwu4"
+const val APPLICATION_NAME = "goya-google-sheet"
 val JSON_FACTORY: JsonFactory = GsonFactory.getDefaultInstance()
-val SCOPES: List<String> = listOf(SheetsScopes.SPREADSHEETS)
-
-class GoogleSheetService {}
+val SCOPES = listOf(SheetsScopes.SPREADSHEETS)
 
 @Throws(IOException::class)
 fun getCredentials(): Credential? {
-	val loader = GoogleSheetService::class.java.classLoader
-	loader.getResourceAsStream(CREDENTIALS_FILE_PATH)?.let {
-		return GoogleCredential.fromStream(it).createScoped(SCOPES)
-	}
-	return null
+	return ClassLoader.getSystemClassLoader()
+		.getResourceAsStream(CREDENTIALS_FILE_PATH)
+		?.let { GoogleCredential.fromStream(it).createScoped(SCOPES) }
 }
 
 fun getGoogleSheetService(): Sheets {
-	val httpTransport: NetHttpTransport = GoogleNetHttpTransport.newTrustedTransport();
-	val service: Sheets =
-		Sheets.Builder(httpTransport, JSON_FACTORY, getCredentials()).setApplicationName(APPLICATION_NAME).build();
-	return service
+	val httpTransport: NetHttpTransport = GoogleNetHttpTransport.newTrustedTransport()
+	return Sheets.Builder(httpTransport, JSON_FACTORY, getCredentials()).setApplicationName(APPLICATION_NAME).build();
 }
 
 /**
@@ -41,16 +35,13 @@ fun getGoogleSheetService(): Sheets {
  */
 fun googleSheetDataLoad(range: String): List<List<Any>> {
 	try {
-		val reponse: ValueRange = getGoogleSheetService().spreadsheets().values().get(SHEET_ID, range).execute()
-		val values: List<List<Any>> = reponse.getValues()
+		val valueRange: ValueRange = getGoogleSheetService().spreadsheets().values().get(SHEET_ID, range).execute()
+		val values: List<List<Any>> = valueRange.getValues()
 		
 		if (values.isEmpty()) {
 			println("No data found.")
 		}
-		
-		println("Data load success.")
 		return values
-		
 	} catch (e: GoogleJsonResponseException) {
 		println(e.statusCode) // 403
 		println(e.statusMessage) // Forbidden
