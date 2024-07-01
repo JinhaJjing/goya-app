78adc672265a796fb1590ca6b6b4585cee7729ec
JinhaJjing <tjwlsgkkgslwjt@gmail.com>
2024-07-01 12:29:15 +0900

    로그 메시지 지움

--- src/main/kotlin/com/group/goyaapp/service/AccountService.kt
+++ src/main/kotlin/com/group/goyaapp/service/AccountService.kt
@@ -33,7 +33,6 @@ class AccountService(
 	fun updateAccountLogin(request: AccountUpdateRequest): AccountResponse {
 		val account =
 			accountRepository.findByAccountIdAndAccountPW(request.id, request.pw) ?: throw Exception("계정이 존재하지 않습니다.")
-		println("계정 로그인 로그 : ${account.accountId} ${account.accountPW} ${account.datetimeLastLogin}")
 		account.datetimeLastLogin = getServerDateTime()
 		accountRepository.save(account)
 		return accountRepository.findByAccountId(account.accountId).let { AccountResponse.of(it!!) }
